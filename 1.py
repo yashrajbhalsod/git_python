938e4ef532e3dfe0ea8e9b64571b3ef77851440f
yashrajbhalsod <yashbhalsod757@gmail.com>
2025-12-29 10:33:11 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("hello world")
+hello world
