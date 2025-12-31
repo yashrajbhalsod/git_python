df7c10123ff1a76bd989f84a3c97ac2e3a2d9890
yashrajbhalsod <yashbhalsod757@gmail.com>
2025-12-31 09:20:55 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,52 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x=10
+print(x)
+10
+a=5
+>>> print(-a)
+-5
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a|b)
+7
+>>> numbers=[1,2,3,4,5,6]
+>>> print(2 in numbers)
+True
+>>> print(5 not in numbers)
+False
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> a=10
+>>> b=12
+>>> print(a+b)
+22
+>>> print(a-b)
+-2
+>>> print(a/b)
+0.8333333333333334
+>>> print(a*b)
+120
