40e2f39dec7a40c40855d54f6c3fdb2aeebd4877
yashrajbhalsod <yashbhalsod757@gmail.com>
2026-01-05 10:31:41 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,25 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> x="hello world"
+>>> print(x)
+hello world
+>>> x="True"
+>>> print(x)
+True
+>>> x=20.5
+>>> print(x)
+20.5
+>>> x=11
+>>> print(x)
+11
+>>> x=["apple","banana","cherry"]
+>>> print(x)
+['apple', 'banana', 'cherry']
+>>> x=["apple","banana","cherry"]
+>>> print(x)
+['apple', 'banana', 'cherry']
+>>> x=
+SyntaxError: invalid syntax
+>>> x={"name":"john","age":"36"}
+>>> print(x)
+{'name': 'john', 'age': '36'}
