44c7b5703b2207baf5a93bd778f820ab7398d062
yashrajbhalsod <yashbhalsod757@gmail.com>
2026-01-21 08:35:40 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,79 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a = int(input("Enter first number: "))
+Enter first number: 5
+b = int(input("Enter second number: "))
+Enter second number: 6
+if a > b:
+        print("Maximum number is:", a)
+else:
+            print("Maximum number is:", b)
+
+            
+Maximum number is: 6
+p = float(input("Enter Principal amount: "))
+Enter Principal amount: 50000
+r = float(input("Enter Rate of Interest: "))
+Enter Rate of Interest: 5
+t = float(input("Enter Time (in years): "))
+Enter Time (in years): 4
+si = (p * r * t) / 100
+print("Simple Interest =", si)
+Simple Interest = 10000.0
+s = input("Enter a string: ")
+Enter a string: 1
+count = 0
+for ch in s:
+        count += 1
+
+        
+print("Length of string =", count)
+Length of string = 1
+print("Welcome")
+Welcome
+s = input("Enter a string: ")
+Enter a string: 5
+print("First character is:", s[0])
+First character is: 5
+s = input("Enter a string: ")
+Enter a string: hello
+print("Last character is:", s[-1])
+Last character is: o
+print(1, 2, 3, 4, 5)
+1 2 3 4 5
+num = int(input("Enter a number: "))
+Enter a number: 5
+if num > 0:
+        print("Number is Positive")
+elif num < 0:
+...             print("Number is Negative")
+... else:
+...     print("Number is Zero")
+... 
+Number is Positive
+>>> 
+>>> a = int(input("Enter first number: "))
+Enter first number: 50
+>>> b = int(input("Enter second number: "))
+Enter second number: 60
+>>> c = int(input("Enter third number: "))
+Enter third number: 70
+>>> a = int(input("Enter first number: "))
+Enter first number: 56
+>>> b = int(input("Enter second number: "))
+Enter second number: 56
+>>> result = a + b
+>>> print("Addition =", result)
+Addition = 112
+>>> num = int(input("Enter a number: "))
+Enter a number: 5
+>>> if num % 2 == 0:
+...         print("Number is Even")
+...         else:
+...             
+SyntaxError: invalid syntax
+>>> if num % 2 == 0:
+...     print("number is even")
+...     else:
+...         
+SyntaxError: invalid syntax
