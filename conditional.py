9707303c6a77b1b8776fa6fd9cae1415fabc5514
yashrajbhalsod <yashbhalsod757@gmail.com>
2026-02-09 10:12:14 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,43 @@
+# 1. Vote
+
+"""age = int(input("Enter your age: "))
+if age >= 18:
+    print("Yes, You are eligible to vote")
+else:
+    print("No, You are not eligible to vote")"""
+
+# 2. Marks
+
+"""marks = int(input("Enter your marks: "))
+if marks>= 90:
+    print("You will get A+.")
+elif marks>=75:
+    print("You will get B+.")
+elif marks>= 35:
+    print("You will get C.")
+else:
+    print("Haha you are failed.")"""
+
+# 3. Odd or Even
+"""value = int(input("Enter your Value in decimal: "))
+if value%2==0:
+    print("Your number is Even Number.")
+else:
+    print("Your value is Odd Value.")"""
+
+# 4. Positive or Negative
+
+"""num = int(input("Enter your Number: "))
+if num > 0:
+    print("Your number is Positive.")
+elif num < 0:
+    print("Your number is Negative.")
+else:
+    print("Your number is Zero.")"""
+
+# 5. License Eligiblity
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are ready to get your license.")
+else:
+    print("First grow up bro, then you will get a license.")
